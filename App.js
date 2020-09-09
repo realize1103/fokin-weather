53a3a26c91b6a506dd9b0c3424e1f697498c2a6d
realize1103 <realize1103@gmail.com>
2020-09-09 23:18:12 +0200

    Getting the Location

--- App.js
+++ App.js
@@ -1,6 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import Loading from "./Loading"
-export default function App() {
-  return <Loading />;
+import Loading from "./Loading";
+import * as Location from "expo-location";
+
+export default class extends React.Component {
+  getLocation = async () => {
+    const location = await Location.getCurrentPositionAsync();
+    console.log(location);
+  }
+
+  componentDidMount() {
+    this.getLocation();
+  }
+  render() {
+    return <Loading />;
+  }
 }
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # fokin-weather
 
 Create weather app with React Native.
+
+if you want to use expo simulate
+- npm start
+
+if you want to use location information
+- expo install expo-loacation
