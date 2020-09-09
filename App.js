eb9a796db76d939365adcfb6cfc89343f682b5bd
realize1103 <realize1103@gmail.com>
2020-09-10 00:12:47 +0200

    Getting the Weather

--- App.js
+++ App.js
@@ -3,10 +3,19 @@ import React from 'react';
 import Loading from "./Loading";
 import * as Location from "expo-location";
 import { Alert } from 'react-native';
+import axios from 'axios';
+
+const API_KEY = "88e51676d67e6e8b94593cf94673b21d";
 
 export default class extends React.Component {
   state = {
     isLoading: true
+  }
+  getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+    );
+    console.log(data);
   }
   getLocation = async () => {
     try {
@@ -15,6 +24,7 @@ export default class extends React.Component {
 
       //console.log(response);
       const { coords: { latitude, longitude } } = await await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude)
       this.setState({ isLoading: false });
       //console.log(coords.latitude, coords.longitude);
 
--- README.md
+++ README.md
@@ -7,3 +7,6 @@ if you want to use expo simulate
 
 if you want to use location information
 - expo install expo-loacation
+
+if you want to get weather data, You have to join here and get API key. 
+- [http](https://openweathermap.org/)
